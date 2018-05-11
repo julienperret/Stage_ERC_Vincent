a869640af25c1178db2b069aad0e340ab7dace21
vidlb <vincent.delbar@orange.fr>
2018-05-11 15:22:40 +0200

    added weight raster computation

--- simulation.py
+++ simulation.py
@@ -16,7 +16,7 @@ def to_array(tif, dtype=None):
         return ds.ReadAsArray().astype(np.float32)
     elif dtype == 'uint16':
         return ds.ReadAsArray().astype(np.uint16)
-    else :
+    else:
         return ds.ReadAsArray()
     ds = None
 
@@ -78,7 +78,7 @@ if len(sys.argv) > 3:
     if mode not in {'souple', 'strict'}:
         print('Mode de seuillage invalide\nValeurs possibles : souple ou strict')
         sys.exit()
-else :
+else:
     mode = 'souple'
 
 finalYear = 2040
@@ -91,7 +91,7 @@ contigDic = {row[0]: row[4] for _, row in irisDf.iterrows()}
 # Projections démograhpiques
 irisDf['population'] = irisDf['population'].astype(int)
 popDf = pd.DataFrame()
-popDf['id'] = [i+1 for i in range(nbIris)]
+popDf['id'] = [i + 1 for i in range(nbIris)]
 for year in range(2014, 2041):
     popDf[year] = 0
 for id in range(nbIris):
@@ -101,28 +101,30 @@ for id in range(nbIris):
     year += 1
     while year <= finalYear:
         popDf[year][id] = pop * (taux / 100)
-        pop +=  pop * (taux / 100)
+        pop += pop * (taux / 100)
         year += 1
 popDf.to_csv('output/demographie.csv', index=0)
 
 # Nombre total de personnes à loger - permet de vérifier si le raster capacité pourra tout contenir
-sumPopALoger = sum(popDf.sum()) - sum(range(nbIris+1)) - sum(popDf[2014])
-print('Population à loger d\'ici à ' + str(finalYear) + ' : ' + str(sumPopALoger))
+sumPopALoger = sum(popDf.sum()) - sum(range(nbIris + 1)) - sum(popDf[2014])
+print('Population à loger d\'ici à ' +
+      str(finalYear) + ' : ' + str(sumPopALoger))
 
 # Calcul des coefficients de pondération de chaque raster d'intérêt, csv des poids dans le répertoire des données locales
 poids = pd.read_csv('../poids.csv')
 poids['coef'] = poids['poids'] / sum(poids['poids'])
-poids.to_csv('output/coef.csv', index=0, columns=['raster', 'coef'])
+poids.to_csv('output/coefficients.csv', index=0, columns=['raster', 'coef'])
 del poids
-dicCoef = {row[0]: row[1] for _, row in pd.read_csv('output/coef.csv').iterrows()}
+dicCoef = {row[0]: row[1]
+           for _, row in pd.read_csv('output/coefficients.csv').iterrows()}
 
 # Création des variables GDAL pour écriture de raster
 ds = gdal.Open('population.tif')
 population = ds.GetRasterBand(1).ReadAsArray().astype(np.uint16)
 cols = ds.RasterXSize
 rows = ds.RasterYSize
-projection = ds.GetProjection()
-geotransform = ds.GetGeoTransform()
+proj = ds.GetProjection()
+geot = ds.GetGeoTransform()
 driver = gdal.GetDriverByName('GTiff')
 ds = None
 
@@ -140,9 +142,18 @@ recreatif = to_array('recreatif.tif', 'float32')
 medical = to_array('medical.tif', 'float32')
 enseignement = to_array('enseignement.tif', 'float32')
 
+# Création du raster final d'intérêt avec pondération
+interet = np.where((restriction != 1), ((ecologie * dicCoef['ecologie']) + (ocsol * dicCoef['ocsol']) + (routes * dicCoef['routes']) + (transport * dicCoef['transport']) + (
+    administratif * dicCoef['administratif']) + (commercial * dicCoef['commercial']) + (recreatif * dicCoef['recreatif']) + (medical * dicCoef['medical']) + (enseignement * dicCoef['enseignement'])), 0)
+to_tif(interet, gdal.GDT_Float32, 'output/interet.tif')
+
 # On vérifie que la capcité d'accueil est suffisante, ici on pourrait modifier la couche de restriction pour augmenter la capacité
 capaciteAccueil = sum(sum(capacite))
 print("Capacité d'accueil du territoire : " + str(capaciteAccueil))
-if capaciteAccueil < sumPopALoger :
+if capaciteAccueil < sumPopALoger:
     print("La capacité d'accueil ne suffit pas pour de telles projections démographiques !")
     sys.exit()
+
+# Itération au pas annuel sur toute la période :
+for year in range(2015, finalYear + 1):
+    popDic = {row[0]: row[year] for _, row in popDf.iterrows()}
