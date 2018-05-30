fb122bdd08e0ac129cef11e3060525f468328fa9
vidlb <vincent.delbar@orange.fr>
2018-05-30 16:35:05 +0200

    finally working

--- majic_to_csv.py
+++ majic_to_csv.py
@@ -31,20 +31,17 @@ if len(sys.argv) > 5:
 if not os.path.exists(outputDir):
     os.makedirs(outputDir)
 
-start_time = time.time()
-print("Commencé à " + time.strftime('%H:%M:%S'))
-
-if 'depList' not in globals():
-    depList = []
-    fileList = os.listdir(inputDir)
-    fileList.sort()
-    for f in fileList:
-        res = None
-        res = re.search('ART\.DC21\.W17([0-9]{2})0\.BATI\.A([0-9]{4})\.N000671', f)
-        if res:
-            dep = res.group(1)
-            if dep not in depList:
-                depList.append(dep)
+def writeHeaders(prefix, tab):
+    with open(prefix + tab + '.csv', 'w') as w:
+        i = 0
+        for field in modelSorted[tab]:
+            i += 1
+            h = field
+            if i < len(modelSorted[tab]):
+                h += ','
+            else:
+                h += '\n'
+            w.write(h)
 
 def getTuple(l, tab):
     i = 0
@@ -64,42 +61,36 @@ def getTuple(l, tab):
         tuple += v
     return tuple + '\n'
 
-# Pour les tables avec enregistrements : traitement d'une ligne et écriture dans le csv
 def writeLine(prefix, tab, line, minLen, eCutList):
-    if eCutList :
-        res = None
-        if len(line) >= minLen:
-            e = line[eCutList[0]:eCutList[1]]
-            res = re.search('[0-9]{2}', e)
-            if res and e in eDic[tab]:
-                with open(prefix + tab + e + '.csv', 'r+') as w:
-                    t = getTuple(line, tab + e)
-                    w.write(t)
+    res = None
+    e = line[eCutList[0]:eCutList[1]]
+    res = re.search('[0-9]{2}', e)
+    if res and e in eDic[tab]:
+        with open(prefix + tab + e + '.csv', 'a') as w:
+            w.write(getTuple(line, tab + e))
 
-# Boucle centrale de lecture d'une table
 def parseTable(prefix, tab, dep):
+    count = 0
     global countLines
     with open(inputDir + 'ART.DC21.W17' + dep + '0.' + tab + '.A2017.N000671', 'r') as r:
-        with mmap.mmap(r.fileno(), 0, access=mmap.ACCESS_READ) as mr:
-            minLen = minLenDic[tab]
-            if tab in eCutDic.keys():
-                eCutList = eCutDic[tab]
-                for line in mr:
-                    countLines += 1
+        minLen = minLenDic[tab]
+        if tab in eCutDic.keys():
+            eCutList = eCutDic[tab]
+            for line in r:
+                count += 1
+                if len(line) >= minLen:
                     writeLine(prefix, tab, line, minLen, eCutList)
-            # Si table sans enregistrements : traitements de n lignes puis écriture
-            else:
-                buffer = []
-                bufferSize = 100000
-                with open(prefix + tab + '.csv', 'r+') as w:
-                    for line in mr:
-                        countLines += 1
-                        if len(line) >= minLen:
-                            w.write(getTuple(line, tab))
+        else:
+            with open(prefix + tab + '.csv', 'a') as w:
+                for line in r:
+                    count += 1
+                    if len(line) >= minLen:
+                        w.write(getTuple(line, tab))
+    countLines += count
 
 # Variables globales
-model = {}
 countLines = 0
+model = {}
 eDic = {
     'BATI': ['00','10','30','36','40','50','60'],
     'NBAT': ['10','21','30','36'],
@@ -118,6 +109,20 @@ if 'tables' not in globals():
 else:
     tables.append(uTable)
 
+if 'depList' not in globals():
+    depList = []
+    fileList = os.listdir(inputDir)
+    fileList.sort()
+    for f in fileList:
+        res = None
+        res = re.search('ART\.DC21\.W17([0-9]{2})0\.BATI\.A([0-9]{4})\.N000671', f)
+        if res:
+            dep = res.group(1)
+            if dep not in depList:
+                depList.append(dep)
+
+start_time = time.time()
+print("Commencé à " + time.strftime('%H:%M:%S'))
 # Création de la structure de recherche des valeurs de découpe {'':{'':[]}}
 for tab in modList:
     with open(modelDir + tab) as file:
@@ -134,35 +139,26 @@ modelSorted = {tab:[field[0] for field in tmpModel[tab]] for tab in tmpModel.key
 del tmpModel
 
 # Variables pour multithreading
-pool = mp.Pool()
+pool = mp.Pool(4)
 jobs = []
 # Itération dans les départements
 for dep in depList:
     prefix = outputDir + dep + '/'
     if not os.path.exists(prefix):
         os.makedirs(prefix)
-    # Ecriture des fichiers avec headers dans le dossier outputDir + dep
-    for tab in model.keys():
-        with open(prefix + tab + '.csv', 'w') as w:
-            i = 0
-            for field in modelSorted[tab]:
-                i += 1
-                h = field
-                if i < len(modelSorted[tab]):
-                    h += ','
-                else:
-                    h += '\n'
-                w.write(h)
-
-    # Ajout des tâches pour multithreading
     for tab in tables:
         jobs.append(pool.apply_async(parseTable,(prefix, tab, dep)))
-
+        if tab in eDic.keys():
+            for e in eDic[tab]:
+                writeHeaders(prefix, tab + e)
+        else:
+            writeHeaders(prefix, tab)
 c = 0
 for j in jobs:
     j.get()
     c += 1
-    print('Taches restantes : ' + str(len(jobs) - c))
+    print('Tâches restantes : ' + str(len(jobs) - c))
+
 pool.close()
 pool.join()
 
