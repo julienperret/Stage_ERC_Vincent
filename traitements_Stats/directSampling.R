f33aff6285724135070ea332ce3d614c7107717c
chapinux <paul.chapron@gmail.com>
2018-07-24 18:20:22 +0200

    exploration visuelle du direct sampling

--- traitements_Stats/directSampling.R
+++ traitements_Stats/directSampling.R
@@ -1,106 +1,104 @@
 library(ggplot2)
 
-setwd("~/encadrement/Stage_ERC_Vincent/traitements_Stats/")
-
-files <-  list.files("./results/", pattern = "mesures.csv", recursive = T, include.dirs = T, full.names = T)
-length(files)
-
-## préparation du tableau final params + mesures
-dd <- data.frame(
-  taux= numeric(),
-  scenario=character(),
-  pluPriority = logical(),
-  buildNonRes = logical(),
-  densifyGround = logical(),
-  maxBuiltRatio = numeric(),
-  densifyOld = logical(),
-  maximumDensity = logical(),
-  winSize =  numeric(),
-  minContig = numeric(),
-  maxContig = numeric(),
-  PopNotPutUp=numeric(),
-  UnbuiltArea = numeric(),
-  AvgCellPop = numeric(),
-  AreaExpansion = numeric(),
-  BuiltFloorArea = numeric(),
-  CellesOpenTiUrb= numeric(),
-  AvgArtifRate = numeric(),
-  impact = numeric(),
-  GrndDensifiedCells = numeric(),
-  FloorDensifiedCells=numeric())
-
-namesbckup <-  names(dd)
 
 
 
+#################################
+# A partir du fichier agrégé
+###################################
 
-for (fifi in files){
-  #on lit le fichier mesures.csv
-  cat(fifi, "\n")
-  dfbrut <-  read.csv(fifi, header = F)
-  namesMesures <- dfbrut$V1
+dd <-  read.csv("directSamplingfulldataframe.csv")
+
+#distribution des impacts 
+pniv <-  ggplot(dd, aes(impact))+
+  geom_histogram( fill="darkolivegreen2", colour="darkgrey", binwidth = 100000 )
+pniv
+
+
+dd$densifyGround
+
+# dynamiques cohérentes avec maxBuilt Ratio et densifyGround
+
+
+#on change le booelan densifyGround en facteurs
+# attention à l'ordre des levels de facteurs : levels(factor(dd$densifyGround))
+dd$densifyGround <- factor(dd$densifyGround, labels=c("Not DensifyGround", "DensifyGround"))
+
+
+pp <-  ggplot(dd, aes(taux,  impact,scenario,maxBuiltRatio,densifyGround))+
+geom_jitter(aes( color=maxBuiltRatio), height = 0.4, size = 0.4 )+
+  facet_grid(rows = vars(scenario), cols = vars(densifyGround))+
+  ggtitle("Impacts par scénarios et choix de densification", subtitle = "valeurs de taux et de maxBuiltRatio uniformément échantillonnées")
+pp
+
+
+
+
+# pour un taux de croissance variant dans une petite fenètre 
+ddd <-  dd %>% filter(between(taux, 1.0, 1.5))
   
-  # valeurs de mesures.csv
-  mes <-  t(dfbrut$V2)
-  mes <-  c(mes)
-  if( length(mes)==8){
-    mes <- c(mes, 0.0,0.0)
-  }
-  if( length(mes)==9){
-    mes <- c(mes, 0.0)
+#histogramme des impacts par taux
+pImpByTaux <-  ggplot(dd, aes(impact))+
+  geom_histogram(aes(fill=factor(taux)), color="gray", binwidth = 200000)+
+  facet_grid(rows = vars(densifyGround))+
+  labs(y="effectif")+
+  scale_fill_discrete("taux")
+
+pImpByTaux
+
+
+
+# pour un taux de croissance variant dans une petite fenètre 
+ddd <-  dd %>% filter(between(taux, 2.5, 3))
+
+pImpByTaux2 <-  ggplot(ddd, aes(impact))+
+  geom_histogram(aes(fill=factor(taux)), color="gray", binwidth = 200000)+
+  facet_grid(rows = vars(densifyGround))+
+  labs(y="effectif")+
+  scale_fill_discrete("taux")
+
+pImpByTaux2
+
+
+
+#pour un quartile d'impact 
+dddd <-  dd %>%  filter(impact > quantile(dd$impact, probs = 0.75))
+
+
+ppQ4 <-  ggplot(dddd, aes(taux,  impact,scenario,maxBuiltRatio,densifyGround))+
+  geom_jitter(aes( color=maxBuiltRatio), height = 0.4, size = 0.4 )+
+  facet_grid(rows = vars(scenario), cols = vars(densifyGround))+
+  ggtitle("Quatrième quartile des Impacts, par scénarios et choix de densification", subtitle = "valeurs de taux et de maxBuiltRatio uniformément échantillonnées")
+ppQ4
+
+
+
+
+
+# on ajoute des variables artificielles
+dd$below75 <-  dd$maxBuiltRatio < 75
+
+
+#on change le booelan densifyGround en facteurs
+# attention à l'ordre des levels de facteurs : 
+dd$below75 <- factor(dd$below75, labels=c("maxBuiltRatio above 75%", "maxBuiltRatio below 75%"))
+
+ppMBR75 <-  ggplot(dd, aes(taux, impact,scenario,below75,densifyGround))+
+      geom_jitter(aes( color=scenario  ), height = 0.2, size=0.4)+
+      facet_grid(rows = vars((below75)), cols = vars(densifyGround))+
+  ggtitle("Régimes d'impact, par choix de densification et maxBuiltRatio ", subtitle = "valeurs de taux et de scénarios uniformément échantillonnées")
+ppMBR75
     
-    }
-  ligneMesure <-  data.frame(t(mes))
-  
-  
-  
-  names(ligneMesure) <-  c("Population not put up","Unbuilt area" ,"Average cell populating","Area expansion","Built floor area",
-                           "Cells open to urbanisation", "Average artificialisation rate", "Cumulated environnemental impact",
-                           "Ground-densified cells count", "Floor-densified cells count" )
-  
-  
-  
-  #valeurs des parametres
-  paramsfifi <-  strsplit(fifi,"_")
-  paramsbruts <-  paramsfifi[[1]][2:11]
-
-  taux <- as.numeric(as.character(paramsbruts[1]))
-  scenario <- paramsbruts[2]
-  pluPriority <-  as.logical(paramsbruts[3])
-  buildNonRes <-  as.logical(paramsbruts[4])
-  densifyGround <-  as.logical(paramsbruts[5])
-  maxBuiltRatio <-  as.numeric(paramsbruts[6])
-  densifyOld <-  as.logical(paramsbruts[7])
-  maximumDensity <-  as.logical(paramsbruts[8])
-  winSize <-   as.numeric(paramsbruts[9])
-  minContig <-  as.numeric(paramsbruts[10])
-  maxContig <-  5
-  
-  
-  #une ligne du futur tableau qui reprend parametres et mesure
-  # on ajoute ContigMax = 5 à la fin 
-  ligneparam <-  data.frame(taux, scenario, pluPriority, buildNonRes, densifyGround, maxBuiltRatio, densifyOld, maximumDensity,winSize, minContig , maxContig)
-  lignetotal <-  cbind(ligneparam, ligneMesure)
-  cat(length(lignetotal),"colonnes \n")
-  
-        dd <-  rbind(dd, lignetotal)
-  
-}
+    
+#autres mesures de sorties : comment se répartit l'impact
 
 
-names(dd) <-  namesbckup
+names(dd)
+ppp <-  ggplot(dd, aes(PopNotPutUp, UnbuiltArea,AvgCellPop,AreaExpansion,,BuiltFloorArea,CellesOpenTiUrb,AvgArtifRate,impact,GrndDensifiedCells))+
+  geom_point()
+ppp
 
-dd$scenario
+mes <-  dd[,7:15]
 
-pp <-  ggplot(dd, aes(taux,  maxBuiltRatio,impact,scenario))+
-geom_jitter(aes(color=impact, shape=scenario), height = 1.9, size=2)
-  pp
-
-
-
-  
-  pp <-  ggplot(dd, aes(  maxBuiltRatio,impact,scenario))+
-    geom_jitter(aes(color=scenario), height = 1.9, size=2)
-  pp
-  
+plot(mes)
 
--- traitements_Stats/profile.R
+++ traitements_Stats/profile.R
@@ -2,9 +2,32 @@ library(ggplot2)
 
 setwd("~/encadrement/repoJulienERC/erc/traitements_Stats/")
 
-df <-  read.csv("population7609.csv")
+df2 <-  read.csv("ProfilMBRsurtauxTendanciel.csv")
+
 
 df <-  read.csv("population862.csv")
+
+
+names(df)
+
+df$pluPriority <-  df$pluPriority > 0.5
+df$buildNonRes <-  df$buildNonRes > 0.5
+df$densifyGround <-  df$densifyGround > 0.5
+df$densifyOld <-  df$densifyOld > 0.5
+df$maximumDensity <-  df$maximumDensity > 0.5
+
+
+df2$pluPriority <-  df2$pluPriority > 0.5
+df2$buildNonRes <-  df2$buildNonRes > 0.5
+df2$densifyGround <-  df2$densifyGround > 0.5
+df2$densifyOld <-  df2$densifyOld > 0.5
+df2$maximumDensity <-  df2$maximumDensity > 0.5
+
+
+
+
+library(dplyr)
+df <-  df %>%  filter(impact < max(impact))
 
 
 names(df)
@@ -13,5 +36,19 @@ names(df)
 plot(df)
 
 pp <-  ggplot(df, aes(maxBuiltRatio, impact))+
-  geom_point()
+ # geom_point(aes(color=buildNonRes, shape=densifyOld))+
+  geom_point(data=df2,aes(color=buildNonRes, shape=densifyOld), size= 2 ) 
+
 pp
+
+
+df3 <- read.csv("ProfilWinSizesurTauxTendanciel.csv")
+
+
+pp <-  ggplot(df3, aes(round(winSize), impact))+
+  geom_point(aes(color=buildNonRes, shape=densifyOld))+
+  geom_point(data=df2,aes(color=buildNonRes, shape=densifyOld), size= 4 ) 
+  geom_line(color="lightgray")+
+  stat_smooth()
+pp
+
