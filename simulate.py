dcea59f004a2ee9fd277509a28d35282ea296cda
vidlb <vincent.delbar@orange.fr>
2018-08-20 01:50:01 +0200

    typo

--- simulate.py
+++ simulate.py
@@ -643,7 +643,7 @@ with (project/'log.txt').open('w') as log, (project/'output/mesures.csv').open('
             to_tif(srfSolNouv, 'uint16', proj, geot, project/'output/surface_sol_construite.tif')
             to_tif(srfPlaNouv, 'uint16', proj, geot, project/'output/surface_plancher_construite.tif')
             to_tif(popNouv, 'uint16', proj, geot, project/'output/population_nouvelle.tif')
-            to_tif(srfSolNonRes, 'uint16', proj, geot, project/('surface_sol_non_residentielle_' + str(finalYear) + '.tif'))
+            to_tif(srfSolNonRes, 'uint16', proj, geot, project/('output/surface_sol_non_residentielle_' + str(finalYear) + '.tif'))
             if exclusionRatio > 0:
                 to_tif(densifSol, 'byte', proj, geot, project/'output/densification_sol.tif')
             if densifyOld:
